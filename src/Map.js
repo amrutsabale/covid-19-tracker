307858e75abffd05b3db9605583d38972bb644e5
amrutsabale <samrut355@gmail.com>
2020-12-22 12:11:57 +0530

    Added css for Popup content

--- src/Map.js
+++ src/Map.js
@@ -7,13 +7,14 @@ import { showDataOnMap } from './util'
 function Map({ countries, casesType, center, zoom }) {
     return (
         <div className="map">
-            <LeafletMap center={center} zoom={zoom} scrollWheelZoom={false}>
+            <LeafletMap center={center} zoom={zoom} >
                 <TileLayer
                     url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreet</a> contributors'
                 />
                 {showDataOnMap(countries, casesType)}
             </LeafletMap>
+
         </div>
     )
 }
