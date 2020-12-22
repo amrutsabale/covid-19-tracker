2807d7b288d3dcb2f2aeb2984a85bbbee674a657
amrutsabale <samrut355@gmail.com>
2020-12-22 12:10:01 +0530

    Added Popup for each Circle

--- src/util.js
+++ src/util.js
@@ -15,7 +15,7 @@ const casesTypeColors = {
     deaths: {
         hex: "#fb4443",
         multiplier: 2000,
-    },
+    }
 }
 
 
@@ -24,8 +24,9 @@ export const sortData = (data) => {
     return sortedData.sort((a, b) => a.cases > b.cases ? -1 : 1);
 }
 
-export const showDataOnMap = (data, casesType = 'cases') => (
+export const showDataOnMap = (data, casesType = "cases") => (
     data.map(country => (
+
         <Circle
             center={[country.countryInfo.lat, country.countryInfo.long]}
             fillOpacity={0.4}
@@ -33,8 +34,18 @@ export const showDataOnMap = (data, casesType = 'cases') => (
             fillColor={casesTypeColors[casesType].hex}
             radius={Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier}
         >
-
+            <Popup>
+                <div className="info-container">
+                    <div className="info-flag" style={{ backgroundImage: `url(${country.countryInfo.flag})` }}></div>
+                    <div className="info-name">{country.country}</div>
+                    <div className="info-confirmed">Cases: {numeral(country.cases).format("0,0")}</div>
+                    <div className="info-recovered">Recovered: {numeral(country.recovered).format("0,0")}</div>
+                    <div className="info-deaths">Deaths: {numeral(country.deaths).format("0,0")}</div>
+                </div>
+            </Popup>
 
         </Circle>
     ))
 )
+
+export const prettyPrint = (stat) => stat ? `+${numeral(stat).format("0.0a")}` : "+0"
